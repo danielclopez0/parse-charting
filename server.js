84c904349c3d58341f3d25ffea4a1d1bf0a195ee
danielclopez0 <danielclopez0@gmail.com>
2016-04-19 22:03:19 -0600

    added react

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var path = require('path');
 
 var config = require(path.join(__dirname, '/private/config.js'));
 
-
 var databaseUri = config.mongo.url || process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
@@ -17,40 +16,38 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
+  appId: 'parseCharts',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  javascriptKey: "1234",
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
 });
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
 var app = express();
 
-// Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
+// Serve static assets from the /app folder
+app.use(express.static(path.join(__dirname, '/dist')));
 
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
-// Parse Server plays nicely with the rest of your web routes
+// send to webpack compiled index.html page
 app.get('/', function(req, res) {
-  res.status(200).send('Make sure to star the parse-server repo on GitHub!');
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
-// There will be a test page available on the /test path
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
+    console.log('parse-charts running on port ' + port + '.');
 });
 
 // This will enable the Live Query real-time server
